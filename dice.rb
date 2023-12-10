3c81e40d38d93d64c8fbe94cbec7ef33e4f1c8c5
Joseph Sieger <jsieger44@gmail.com>
2023-12-10 19:26:08 +0000

    changed wapper to layout, we dont need the erb extra arg now

--- dice.rb
+++ dice.rb
@@ -27,7 +27,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six, {:layout => :wrapper})
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -37,7 +37,7 @@ get("/dice/2/10") do
 
   @outcome = "you rolled a #{first_die} on the first die and a #{second_die} on the second die, the sum is #{sum}"
 
-  erb(:two_ten, {:layout => :wrapper})
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
@@ -45,7 +45,7 @@ get("/dice/1/20") do
 
   @outcome = "there was only one die and it reads #{first_die}"
 
-  erb(:one_twenty, {:layout => :wrapper})
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -58,5 +58,5 @@ get("/dice/5/4") do
 
   @outcome = "you rolled a #{first_die} , #{second_die} , #{third_die} , #{fourth_die} , the sum is #{sum}"
 
-  erb(:five_four, {:layout => :wrapper})
+  erb(:five_four)
 end
