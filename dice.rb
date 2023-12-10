faea512e6945136b3cd89718149193f2cd111362
Joseph Sieger <jsieger44@gmail.com>
2023-12-10 19:34:01 +0000

    if statements in the template

--- dice.rb
+++ dice.rb
@@ -41,9 +41,9 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @first_die = rand(1..20)
 
-  @outcome = "there was only one die and it reads #{first_die}"
+  @outcome = "there was only one die and it reads #{@first_die}"
 
   erb(:one_twenty)
 end
