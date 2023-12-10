8125ade02dc3bfaecf8d73c581cf5dc4ba68aada
Joseph Sieger <jsieger44@gmail.com>
2023-12-10 19:17:13 +0000

    put all the boilerplate in wrapper

--- dice.rb
+++ dice.rb
@@ -27,7 +27,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six)
+  erb(:two_six, {:layout => :wrapper})
 end
 
 get("/dice/2/10") do
@@ -37,8 +37,7 @@ get("/dice/2/10") do
 
   @outcome = "you rolled a #{first_die} on the first die and a #{second_die} on the second die, the sum is #{sum}"
 
-  erb(:two_ten)
-
+  erb(:two_ten, {:layout => :wrapper})
 end
 
 get("/dice/1/20") do
@@ -46,7 +45,7 @@ get("/dice/1/20") do
 
   @outcome = "there was only one die and it reads #{first_die}"
 
-  erb(:one_twenty)
+  erb(:one_twenty, {:layout => :wrapper})
 end
 
 get("/dice/5/4") do
@@ -59,5 +58,5 @@ get("/dice/5/4") do
 
   @outcome = "you rolled a #{first_die} , #{second_die} , #{third_die} , #{fourth_die} , the sum is #{sum}"
 
-  erb(:five_four)
+  erb(:five_four, {:layout => :wrapper})
 end
